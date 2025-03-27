de70b943eb16471a9d7d76cd50a6a4ca3140d5c7
brunotxrs <elielsonbrunno@gmail.com>
2025-03-27 15:06:39 -0400

    feat: import the component HomeScreen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from 'react';
+import HomeScreen from './components/homescreen/HomeScreen';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <HomeScreen />
     </div>
   );
 }
