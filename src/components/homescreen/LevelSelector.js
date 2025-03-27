934c6bda4a914e16555c193ba2280a22b191b470
brunotxrs <elielsonbrunno@gmail.com>
2025-03-27 18:22:15 -0400

    import useContext and PlayerContext

--- src/components/homescreen/LevelSelector.js
+++ src/components/homescreen/LevelSelector.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import '../homescreen/HomeScreen.css'
+import { PlayerContext } from '../contexts/PlayerContext'
 
 
 function LevelSelector() {
 
+    const { setLevel} = useContext(PlayerContext);
+
+    const handleLevelChange = (event) => {
+        setLevel(event.target.value);
+    };
 
     return(
         <div className="container-level-selector">
@@ -12,17 +18,18 @@ function LevelSelector() {
             <div className="box-level">
 
                 <label htmlFor='easy'>
-                    <input type="radio" name="level-selector" value='easy' id="easy"/>
+                    <input type="radio" name="level-selector" value='easy' id="easy"
+                    onChange={handleLevelChange} />
                     <span>Easy</span>     
                 </label>
 
                 <label htmlFor='medium'>
-                    <input type="radio" name="level-selector" value='medium' id="medium"/>
+                    <input type="radio" name="level-selector" value='medium' id="medium" onChange={handleLevelChange} />
                     <span>Medium</span>     
                 </label>
 
                 <label htmlFor='hard'>
-                    <input type="radio" name="level-selector" value='hard' id="hard"/>
+                    <input type="radio" name="level-selector" value='hard' id="hard" onChange={handleLevelChange} />
                     <span>Hard</span>     
                 </label>
             </div>
