eb4cae0e41d99c5926c74c7c4a67ebb087f93bd6
brunotxrs <elielsonbrunno@gmail.com>
2025-03-27 18:17:56 -0400

    create new state

--- src/components/contexts/PlayerContext.js
+++ src/components/contexts/PlayerContext.js
@@ -5,10 +5,11 @@ export const PlayerContext = createContext();
 
 export const PlayerProvider = ({children}) => {
     const [ playerName, setPlayerName] = useState('');
-    const [ nameError, SetNameError ] = useState(''); 
+    const [ nameError, SetNameError ] = useState('');
+    const [ level , setLevel] = useState('');
 
     return (
-        <PlayerContext.Provider value={{playerName, setPlayerName, nameError, SetNameError}}>
+        <PlayerContext.Provider value={{ playerName, setPlayerName, nameError, SetNameError, level,setLevel }}>
             {children}
         </PlayerContext.Provider>
     );
