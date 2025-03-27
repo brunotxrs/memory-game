c293bcf36cfaa73ca11df97b481a8bf93785230b
brunotxrs <elielsonbrunno@gmail.com>
2025-03-27 19:39:19 -0400

    feat: import GameScreen

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import HomeScreen from './components/homescreen/HomeScreen';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import GameScreen from './components/gamescreen/GameScreen';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<HomeScreen />} />
+      <Route path='/gamescreen' element={<GameScreen />} />
       
     </Routes>
     </BrowserRouter>
