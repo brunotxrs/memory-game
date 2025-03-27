53ababaf8ce9058481d1451562c362c16cff6726
brunotxrs <elielsonbrunno@gmail.com>
2025-03-27 16:21:29 -0400

    feat: import useContext and import PlayerContext

--- src/components/homescreen/NameInput.js
+++ src/components/homescreen/NameInput.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './HomeScreen.css'
+import { PlayerContext } from '../contexts/PlayerContext'
 
 function NameInput() {
+
+    const { setPlayerName, SetNameError, nameError } = useContext(PlayerContext);
+
+    const handleInputChange = (event) => {
+        const name = event.target.value;
+        setPlayerName(name);
+        if(name.length === 0) {
+            SetNameError('')
+        } else if (name.length < 3) {
+            SetNameError('O nome deve ter no mínimo 3 caracteres.');
+        } else if (name.length > 5) {
+            SetNameError('O nome deve ter no máximo 5 caracteres.');
+        } else {
+            SetNameError('');
+        }
+    }
+
     return (
         <div className="container-name-input">
-            <input type="text" placeholder="name" autoComplete="off" translate="no" />
+            <div className='box-error'>{nameError && <p className="error-message">{nameError}</p>}</div>
+
+            <div className='box-input'><input type="text" placeholder="name" autoComplete="off" translate="no" onChange={handleInputChange}/></div>
+            
         </div>
     );
 }
