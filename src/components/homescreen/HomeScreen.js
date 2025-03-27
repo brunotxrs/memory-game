a08379e1e1c809e250ae906ea1d05dc44e28a8b4
brunotxrs <elielsonbrunno@gmail.com>
2025-03-27 16:20:50 -0400

    feat: import useContext and useEffect and import PlayerContext

--- src/components/homescreen/HomeScreen.js
+++ src/components/homescreen/HomeScreen.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import './HomeScreen.css'
 import logo from '../../assets/img/jogo da memoria.png'
 import LevelSelector from "./LevelSelector";
 import NameInput from "./NameInput";
 import StartButton from "./StartButton";
+import { PlayerContext } from '../contexts/PlayerContext'
 
 function HomeScreen() {
+
+    const { playerName } = useContext(PlayerContext);
+
+    useEffect(() => {
+        console.log("Nome do jogador:", playerName);
+    }, [playerName]);
+
     return (
         <div className="container-homescreen">
             <div className="box-logo">
                 <img className="logo" src={logo} alt="logo Jogo de Memoria"/>
             </div>
-            <NameInput />
-            <LevelSelector />
-            <StartButton />
+            <div className="box-components">
+                <NameInput />
+                <LevelSelector />
+                <StartButton />
+            </div>
         </div>
     );
 }
