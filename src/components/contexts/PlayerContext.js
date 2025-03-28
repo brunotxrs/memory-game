5ace85a457979e89afb448cc35c258bca61ea0a0
brunotxrs <elielsonbrunno@gmail.com>
2025-03-28 10:40:36 -0400

    feat: add new context areclasseAdicional, setClasseAdicional

--- src/components/contexts/PlayerContext.js
+++ src/components/contexts/PlayerContext.js
@@ -7,9 +7,10 @@ export const PlayerProvider = ({children}) => {
     const [ playerName, setPlayerName] = useState('');
     const [ nameError, SetNameError ] = useState('');
     const [ level , setLevel] = useState('');
+    const [ classeAdicional, setClasseAdicional ] = useState('');
 
     return (
-        <PlayerContext.Provider value={{ playerName, setPlayerName, nameError, SetNameError, level,setLevel }}>
+        <PlayerContext.Provider value={{ playerName, setPlayerName, nameError, SetNameError, level,setLevel, classeAdicional, setClasseAdicional }}>
             {children}
         </PlayerContext.Provider>
     );
