f2bec21b6af3e08e1aebcc7055b44edc19718974
brunotxrs <elielsonbrunno@gmail.com>
2025-03-27 23:57:04 -0400

    feat: import useContext, useEffect, useState, PlayerContext, FontAwesomeIcon

--- src/components/gamescreen/Dashboard.js
+++ src/components/gamescreen/Dashboard.js
@@ -1,9 +1,58 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import '../gamescreen/GameScreen.css'
-
+import { PlayerContext } from '../../components/contexts/PlayerContext'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faStopwatch, faUserAstronaut } from "@fortawesome/free-solid-svg-icons";
 
 function DaschboardGame() {
+
+    const { playerName , level} = useContext(PlayerContext);
+    const [ timeRemaining,  setTimeRemaining ] = useState(0);
+    const [ lives,  setLives ] = useState(0);
+
+    useEffect(() => {
+        if(timeRemaining > 0){
+
+            const time = setInterval(() => {
+                setTimeRemaining(timeRemaining -1);
+
+                
+            }, 1000);
+
+            return () => clearInterval(time);
+        }
+    }, [timeRemaining]);
+
+    useEffect(() => {
+        if(level === 'easy') {
+            setLives(2);
+            setTimeRemaining(15);
+        } else if ( level === 'medium') {
+            setLives(4);
+            setTimeRemaining(25);
+        } else if (level === 'hard') {
+            setLives(8);
+            setTimeRemaining(60);
+        } else {
+            setLives('')
+            setTimeRemaining('');
+        };
+    }, [level]);
+
+
+    const formatTime  = (time) => {
+        const minutes = Math.floor(time / 60);
+        const seconds = time % 60;
+
+        return `${minutes.toString().padStart('0')}:${seconds.toString().padStart(2, '0')}`
+
+    };
+
+    const optionLevel = () => {
+
+    }
+
 
 
     return (
@@ -12,18 +61,18 @@ function DaschboardGame() {
 
             <div className="box-dashboard">
                 <div>
-                    <span>ico</span>
-                    <span>Name</span>
+                    <span><FontAwesomeIcon icon={faUserAstronaut}/></span>
+                    <span>{playerName}</span>
                 </div>
 
                 <div>
-                    <span>icoTimer</span>
-                    <span>Number</span>
+                    <span><FontAwesomeIcon icon={faStopwatch}/></span>
+                    <span>{formatTime(timeRemaining)}</span>
                 </div>
 
                 <div>
-                    <span>icoLife</span>
-                    <span>X Number</span>
+                    <span><FontAwesomeIcon icon={faHeart} /></span>
+                    <span>{lives}</span>
                 </div>
             </div>
 
