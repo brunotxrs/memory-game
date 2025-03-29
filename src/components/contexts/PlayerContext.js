6ed558dee5bdfd652105823ee4fdeea75a9c2006
brunotxrs <elielsonbrunno@gmail.com>
2025-03-29 16:24:51 -0400

    feat: add new states

--- src/components/contexts/PlayerContext.js
+++ src/components/contexts/PlayerContext.js
@@ -11,9 +11,12 @@ export const PlayerProvider = ({children}) => {
     const [ lives, setLives] = useState(0);
     const [ timeRemaining, setTimeRemaining ] = useState(0);
     const [ gameOver, setGameOver ] = useState(false);
+    const [ point , setPoint ] = useState(0);
+    const [ pairsMatched, setPairsMatched ] = useState(0);
+    const [ bonus ,  setBonus ] = useState(0)
 
     return (
-        <PlayerContext.Provider value={{ playerName, setPlayerName, nameError, SetNameError, level,setLevel, classeAdicional, setClasseAdicional, score, setScore, lives, setLives, timeRemaining, setTimeRemaining, gameOver, setGameOver }}>
+        <PlayerContext.Provider value={{ playerName, setPlayerName, nameError, SetNameError, level,setLevel, classeAdicional, setClasseAdicional, score, setScore, lives, setLives, timeRemaining, setTimeRemaining, gameOver, setGameOver, point , setPoint, pairsMatched, setPairsMatched, bonus ,  setBonus }}>
             {children}
         </PlayerContext.Provider>
     );
