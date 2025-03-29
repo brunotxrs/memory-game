18c8fb77beb7eaeabc4de0baa1daad2f0466e727
brunotxrs <elielsonbrunno@gmail.com>
2025-03-29 00:16:22 -0400

    feat: create new states

--- src/components/contexts/PlayerContext.js
+++ src/components/contexts/PlayerContext.js
@@ -2,15 +2,18 @@ import { createContext, useState } from "react";
 
 export const PlayerContext = createContext();
 
-
 export const PlayerProvider = ({children}) => {
     const [ playerName, setPlayerName] = useState('');
     const [ nameError, SetNameError ] = useState('');
-    const [ level , setLevel] = useState('');
+    const [ level , setLevel ] = useState('');
     const [ classeAdicional, setClasseAdicional ] = useState('');
+    const [ score, setScore ] = useState(0);
+    const [ lives, setLives] = useState(0);
+    const [ timeRemaining, setTimeRemaining ] = useState(0);
+    const [ gameOver, setGameOver ] = useState(false);
 
     return (
-        <PlayerContext.Provider value={{ playerName, setPlayerName, nameError, SetNameError, level,setLevel, classeAdicional, setClasseAdicional }}>
+        <PlayerContext.Provider value={{ playerName, setPlayerName, nameError, SetNameError, level,setLevel, classeAdicional, setClasseAdicional, score, setScore, lives, setLives, timeRemaining, setTimeRemaining, gameOver, setGameOver }}>
             {children}
         </PlayerContext.Provider>
     );
