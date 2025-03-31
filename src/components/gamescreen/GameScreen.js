324ce5957d3f564b11efd66d78b690ae392996de
brunotxrs <elielsonbrunno@gmail.com>
2025-03-31 12:18:22 -0400

    feat: add song of background

--- src/components/gamescreen/GameScreen.js
+++ src/components/gamescreen/GameScreen.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 import '../gamescreen/GameScreen.css'
 import DashboardGame from "./Dashboard";
 import Game from "./Game";
+import musicBackground from '../../assets/music/background-song.mp3'
 
 
 
 function GameScreen() {
+    const gameAudioRef = useRef(new Audio(musicBackground));
+
+    useEffect(() => {
+        const gameAudio = gameAudioRef.current;
+
+        const playAudio = () => {
+            gameAudio.loop = true;
+            gameAudio.play().catch(error => {
+                console.error("Erro ao reproduzir áudio do jogo:", error);
+            });
+        };
+
+        const pauseAudio = () => {
+            gameAudio.pause();
+            gameAudio.currentTime = 0;
+
+        };
+
+        playAudio();
+
+        return () => {
+            pauseAudio()
+        };
+
+
+    }, []);
 
 
     return (
